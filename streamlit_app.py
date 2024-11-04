53f972205c0ecea274ffd1a86954ae7fa0d184c3
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-11-04 12:52:28 +0000

    Prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Airline experience")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+prompt = st.text_input("Share with us your experience of the latest trip. ")
+
